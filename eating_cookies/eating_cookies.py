7b37774f59ccf3f5193f5c10cdb948f1a3144c65
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-06-17 16:47:03 -0600

    4/5 complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,16 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    CL = [0 for i in range(0, n + 1)] 
+    CL[0] = CL[1] = CL[2] = 1
+    CL[3] = 2
+  
+    for i in range(3, n + 1): 
+        CL[i] = CL[i - 1] + CL[i - 2] + CL[i - 3] 
+    return CL[n] 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
