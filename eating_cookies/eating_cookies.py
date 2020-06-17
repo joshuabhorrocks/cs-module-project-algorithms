f163c1e4b88c0f4249118497a702fab2dbc6bdea
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-06-17 17:34:27 -0600

    Passes in function test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,9 @@ Returns: an integer
 '''
 def eating_cookies(n):
     CL = [0 for i in range(0, n + 1)] 
-    CL[0] = CL[1] = CL[2] = 1
-    CL[3] = 2
+    CL[0] = CL[1] = 1
+    CL[2] = 2
+    CL[3] = 3
   
     for i in range(3, n + 1): 
         CL[i] = CL[i - 1] + CL[i - 2] + CL[i - 3] 
@@ -13,6 +14,22 @@ def eating_cookies(n):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 3
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
+
+# 1+1+1+1+1
+# 1+1+1+2
+# 1+1+2+1
+# 1+2+1+1
+# 2+1+1+1
+# 2+2+1
+# 2+1+2
+# 1+2+2
+# 3+1+1
+# 1+3+1
+# 1+1+3
+# 2+3
+# 3+2
+
+# 13 ways
