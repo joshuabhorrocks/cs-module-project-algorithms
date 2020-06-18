d5a7ed3c4033140aaba763febe3705827f3b243e
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-06-18 13:23:58 -0600

    All tests run quickly

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,9 +4,15 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
-
+    max = 0
+    rMax = []
+    for i in range(len(nums) - k + 1): 
+        max = nums[i] 
+        for j in range(1, k): 
+            if nums[i + j] > max: 
+                max = nums[i + j] 
+        rMax.append(max)
+    return rMax
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
